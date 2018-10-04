0ed4c29eec04ad683c4d88e18de2bcc0b35dcdef
Adam Grare <agrare@redhat.com>
2018-10-04 15:22:20 -0400

    Use DATABASE_URL if it is available

--- lib/ar_helper.rb
+++ lib/ar_helper.rb
@@ -8,5 +8,5 @@ Dir[File.expand_path("app/models/**/*.rb", app_root)].each { |f| require f }
 app_env = ENV["TOPOLOGICAL_INVENTORY_ENV"] || "development"
 
 require "yaml"
-connection_spec = YAML.load_file(File.expand_path("config/database.yml", app_root))
-ActiveRecord::Base.establish_connection(connection_spec[app_env])
+connection_spec = ENV["DATABASE_URL"] || YAML.load_file(File.expand_path("config/database.yml", app_root))[app_env]
+ActiveRecord::Base.establish_connection(connection_spec)
