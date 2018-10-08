d8f1fabb6963fa482fefa006f1e86c4e8291ffbc
Adam Grare <agrare@redhat.com>
2018-10-08 11:20:55 -0400

    Fix issue with running db:migrate from main app

--- lib/topological_inventory/engine.rb
+++ lib/topological_inventory/engine.rb
@@ -1,5 +1,13 @@
 module TopologicalInventory
   class Engine < ::Rails::Engine
     isolate_namespace TopologicalInventory
+
+    initializer :append_migrations do |app|
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        app.config.paths["db/migrate"] << expanded_path
+      end
+      ActiveRecord::Migrator.migrations_paths = app.config.paths["db/migrate"]
+      ActiveRecord::Tasks::DatabaseTasks.migrations_paths = app.config.paths["db/migrate"].to_a
+    end
   end
 end
