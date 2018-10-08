2aef9624ce8e8a293a99de5f7c7ab6d7be769c8c
Adam Grare <agrare@redhat.com>
2018-10-08 11:34:54 -0400

    Fix app path checking

--- lib/topological_inventory/engine.rb
+++ lib/topological_inventory/engine.rb
@@ -3,11 +3,13 @@ module TopologicalInventory
     isolate_namespace TopologicalInventory
 
     initializer :append_migrations do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path
+      unless app.root.to_s == root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+        ActiveRecord::Migrator.migrations_paths = app.config.paths["db/migrate"]
+        ActiveRecord::Tasks::DatabaseTasks.migrations_paths = app.config.paths["db/migrate"].to_a
       end
-      ActiveRecord::Migrator.migrations_paths = app.config.paths["db/migrate"]
-      ActiveRecord::Tasks::DatabaseTasks.migrations_paths = app.config.paths["db/migrate"].to_a
     end
   end
 end
