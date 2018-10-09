0d4581b167dff656742d751d42ae68ec983f69dd
Adam Grare <agrare@redhat.com>
2018-10-09 09:21:54 -0400

    Use concurrent_safe_batch by default

--- lib/topological_inventory/schema/default.rb
+++ lib/topological_inventory/schema/default.rb
@@ -8,17 +8,24 @@ module TopologicalInventory
           builder.add_properties(
             :manager_ref    => [:source_ref],
             :secondary_refs => {:by_name => [:name]},
-            :targeted       => true,
+            :saver_strategy => :concurrent_safe_batch,
           )
           builder.add_default_values(:source_id => ->(persister) { persister.manager.id })
         end
 
         %i(container_groups container_templates service_offerings service_instances service_parameters_sets).each do |model|
           add_collection(model) do |builder|
-            builder.add_properties(:manager_ref => [:source_ref], :targeted => true)
+            builder.add_properties(
+              :manager_ref => [:source_ref],
+              :saver_strategy => :concurrent_safe_batch,
+            )
             builder.add_default_values(:source_id => ->(persister) { persister.manager.id })
           end
         end
+      end
+
+      def targeted?
+        true
       end
     end
   end
