297c6d026440a1671ce8b9a1d9b0ae1d28959b88
Adam Grare <agrare@redhat.com>
2018-10-19 14:16:28 -0400

    Add test for lazy_ref evaluation

--- spec/persister/worker_spec.rb
+++ spec/persister/worker_spec.rb
@@ -1,23 +1,54 @@
 require "topological_inventory/persister/worker"
 
 describe TopologicalInventory::Persister::Worker do
-  let(:tenant) { Tenant.create! }
+  let(:tenant) { Tenant.find_or_create_by!(:name => "default") }
   let(:client) { double(:client) }
-  let!(:source) { Source.create!(:name => "OCP", :uid => "9a874712-9a55-49ab-a46a-c823acc35503", :tenant => tenant) }
+  let!(:source) { Source.find_or_create_by!(:name => "OCP", :uid => "9a874712-9a55-49ab-a46a-c823acc35503", :tenant => tenant) }
 
   context "#run" do
     before do
       allow(ManageIQ::Messaging::Client).to receive(:open).and_return(client)
       allow(client).to receive(:close)
       allow(client).to receive(:subscribe_topic).and_yield(nil, nil, inventory)
+
+      described_class.new.run
+      source.reload
     end
 
     context "with a simple payload" do
       let(:inventory) { JSON.load(File.read(File.expand_path(File.join("test_inventory", "simple_inventory.json"), __dir__))) }
 
       it "saves the container group" do
-        described_class.new.run
-        expect(source.reload.container_projects.count).to eq(1)
+        expect(source.container_projects.count).to eq(1)
+        expect(source.container_projects.first).to have_attributes(
+          :source_ref       => "c56c6854-baaf-11e8-ba7e-d094660d31fb",
+          :name             => "default",
+          :display_name     => "Default",
+          :resource_version => "42001363",
+        )
+      end
+
+      it "sets the tenant to the source's tenant" do
+        expect(source.container_projects.first.tenant).to eq(source.tenant)
+      end
+    end
+
+    context "with some inter-object relationships" do
+      let(:inventory) { JSON.load(File.read(File.expand_path(File.join("test_inventory", "relationships_inventory.json"), __dir__))) }
+
+      it "saves the different inventory objects" do
+        expect(source.container_projects.count).to eq(1)
+        expect(source.container_nodes.count).to    eq(1)
+        expect(source.container_groups.count).to   eq(1)
+      end
+
+      it "saves the relationships between objects" do
+        container_group = source.container_groups.first
+        container_project = source.container_projects.first
+        container_node = source.container_nodes.first
+
+        expect(container_group.container_project).to eq(container_project)
+        expect(container_group.container_node).to eq(container_node)
       end
     end
   end
