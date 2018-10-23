19c61fc878a94f6c06ca69d3011ec1fad14a4a3f
Brandon Dunne <brandondunne@hotmail.com>
2018-10-23 18:06:16 -0400

    Ensure that Endpoint#default is defaulted to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023094055) do
+ActiveRecord::Schema.define(version: 20181023215716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20181023094055) do
     t.bigint "source_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "default"
+    t.boolean "default", default: false
     t.string "scheme"
     t.string "host"
     t.string "path"
