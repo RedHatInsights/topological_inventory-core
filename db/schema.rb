bb3300d36a12d8df5994f465c31009045ba6d4f8
Adam Grare <agrare@redhat.com>
2018-10-24 13:33:15 -0400

    Add a SourceType model for dynamic provider types
    Add a model to track the possible types of sources as well as their
    json schema for creation and update.

--- db/schema.rb
+++ db/schema.rb
@@ -230,12 +230,23 @@ ActiveRecord::Schema.define(version: 20181102145252) do
     t.index ["source_id"], name: "index_source_regions_on_source_id"
   end
 
+  create_table "source_types", force: :cascade do |t|
+    t.string "name"
+    t.string "product_name"
+    t.string "vendor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_source_types_on_name", unique: true
+  end
+
   create_table "sources", force: :cascade do |t|
     t.string "name"
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "tenant_id", null: false
+    t.bigint "source_type_id"
+    t.index ["source_type_id"], name: "index_sources_on_source_type_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
