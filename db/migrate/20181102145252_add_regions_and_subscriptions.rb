2b23dfc07983756e3c85859aea12fccb2801e320
Ladislav Smola <lsmola@redhat.com>
2018-11-06 09:06:17 +0100

    Simplify indexes and foreign keys

--- db/migrate/20181102145252_add_regions_and_subscriptions.rb
+++ db/migrate/20181102145252_add_regions_and_subscriptions.rb
@@ -1,7 +1,7 @@
 class AddRegionsAndSubscriptions < ActiveRecord::Migration[5.1]
   def change
     create_table "subscriptions", :id => :bigserial do |t|
-      t.references "source", :type => :bigint, :index => true, :null => false
+      t.references "source", :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
       t.string     "source_ref"
       t.string     "name"
       t.timestamps
@@ -9,11 +9,9 @@ class AddRegionsAndSubscriptions < ActiveRecord::Migration[5.1]
       t.index      %i(source_id source_ref), :unique => true
     end
 
-    add_foreign_key :subscriptions, :sources, on_delete: :cascade
-
     create_table "source_regions", :id => :bigserial do |t|
-      t.references "source", :type => :bigint, :index => true, :null => false
-      t.references "subscription", :type => :bigint, :index => true
+      t.references "source", :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references "subscription", :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
       t.string     "source_ref"
       t.string     "name"
       t.string     "endpoint"
@@ -22,34 +20,13 @@ class AddRegionsAndSubscriptions < ActiveRecord::Migration[5.1]
       t.index      %i(source_id source_ref), :unique => true
     end
 
-    add_foreign_key :source_regions, :sources, on_delete: :cascade
-    add_foreign_key :source_regions, :subscriptions, on_delete: :cascade
+    add_reference :service_offerings, :source_region, :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
+    add_reference :service_offerings, :subscription, :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
+  
+    add_reference :service_plans, :source_region, :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
+    add_reference :service_plans, :subscription, :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
 
-    # Relations to service_offerings
-    add_column :service_offerings, :source_region_id, :bigint
-    add_index :service_offerings, :source_region_id
-    add_foreign_key :service_offerings, :source_regions, on_delete: :cascade
-
-    add_column :service_offerings, :subscription_id, :bigint
-    add_index :service_offerings, :subscription_id
-    add_foreign_key :service_offerings, :subscriptions, on_delete: :cascade
-
-    # Relations to service_plans
-    add_column :service_plans, :source_region_id, :bigint
-    add_index :service_plans, :source_region_id
-    add_foreign_key :service_plans, :source_regions, on_delete: :cascade
-
-    add_column :service_plans, :subscription_id, :bigint
-    add_index :service_plans, :subscription_id
-    add_foreign_key :service_plans, :subscriptions, on_delete: :cascade
-
-    # Relations to service_instances
-    add_column :service_instances, :source_region_id, :bigint
-    add_index :service_instances, :source_region_id
-    add_foreign_key :service_instances, :source_regions, on_delete: :cascade
-
-    add_column :service_instances, :subscription_id, :bigint
-    add_index :service_instances, :subscription_id
-    add_foreign_key :service_instances, :subscriptions, on_delete: :cascade
+    add_reference :service_instances, :source_region, :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
+    add_reference :service_instances, :subscription, :type => :bigint, :index => true, :foreign_key => {:on_delete => :cascade}
   end
 end
