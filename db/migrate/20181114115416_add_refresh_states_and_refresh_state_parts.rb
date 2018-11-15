3872ddcaba0b58f68460cceaf96b26f883768382
Ladislav Smola <lsmola@redhat.com>
2018-11-15 17:25:11 +0100

    Add refresh_states and refresh_state_parts
    These table will be used for tracking when all parts of full refresh
    were saved. After that, we can do sweep phase of mark&sweep
    alghorithm.

---
+++ db/migrate/20181114115416_add_refresh_states_and_refresh_state_parts.rb
@@ -0,0 +1,25 @@
+class AddRefreshStatesAndRefreshStateParts < ActiveRecord::Migration[5.1]
+  def change
+    create_table "refresh_states", :id => :bigserial do |t|
+      t.references "source", :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references "tenant", :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.uuid       "uuid", :null => false
+      t.string     "status"
+      t.integer    "total_parts"
+      t.jsonb      "sweep_scope"
+      t.integer    "sweep_retry_count", :default => 0
+      t.string     "error_message"
+      t.timestamps
+      t.index      %i(source_id uuid), :unique => true
+    end
+
+    create_table "refresh_state_parts", :id => :bigserial do |t|
+      t.references "refresh_state", :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.uuid       "uuid", :null => false
+      t.string     "status"
+      t.string     "error_message"
+      t.timestamps
+      t.index      %i(refresh_state_id uuid), :unique => true
+    end
+  end
+end
