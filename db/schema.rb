f217e7cee3b3b6d1f2a3feb734818f4ce6851b78
Adam Grare <agrare@redhat.com>
2018-11-28 09:13:55 -0500

    Add json schema to source_types for source create
    Add a json schema for the form for source creation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181128101451) do
+ActiveRecord::Schema.define(version: 20181128141221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -370,6 +370,7 @@ ActiveRecord::Schema.define(version: 20181128101451) do
     t.string "vendor", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "schema"
     t.index ["name"], name: "index_source_types_on_name", unique: true
   end
 
