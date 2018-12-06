69a1727f778088eda9e527073f7ee137ceadfe9c
Ladislav Smola <lsmola@redhat.com>
2018-12-06 18:28:11 +0100

    Make source region optional

--- app/models/volume.rb
+++ app/models/volume.rb
@@ -5,7 +5,7 @@ class Volume < ApplicationRecord
 
   belongs_to :tenant
   belongs_to :source
-  belongs_to :source_region
+  belongs_to :source_region, :optional => true
   belongs_to :volume_type, :optional => true
 
   has_many :volume_attachments
