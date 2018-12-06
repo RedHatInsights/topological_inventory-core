4d03d65b5f8ef3ae020f0b5ebc88fda4604784c2
Erik Clarizio <eclarizi@redhat.com>
2018-12-06 09:48:56 -0800

    Rework parameters for sending namespace and service_parameters

--- spec/topological_inventory/core/service_plan_client_spec.rb
+++ spec/topological_inventory/core/service_plan_client_spec.rb
@@ -4,18 +4,25 @@ module TopologicalInventory
   module Core
     describe ServicePlanClient do
       describe "#build_payload" do
+        let(:order_parameters) do
+          {
+            :service_parameters => service_parameters,
+            :provider_control_parameters => {:namespace => "namespace"}
+          }
+        end
+        let(:service_parameters) { "service_parameters" }
         let(:expected_payload) do
           {
             "apiVersion" => "servicecatalog.k8s.io/v1beta1",
             "kind"       => "ServiceInstance",
             "metadata"   => {
               "name"      => "service_offering_name-uuid",
-              "namespace" => "default"
+              "namespace" => "namespace"
             },
             "spec"       => {
               "clusterServiceClassExternalName" => "service_offering_name",
               "clusterServicePlanExternalName"  => "service_plan_name",
-              "parameters"                      => "catalog_parameters"
+              "parameters"                      => service_parameters
             }
           }.to_json
         end
@@ -26,7 +33,7 @@ module TopologicalInventory
 
         it "returns the payload with the right data" do
           expect(
-            subject.build_payload("service_plan_name", "service_offering_name", "catalog_parameters")
+            subject.build_payload("service_plan_name", "service_offering_name", order_parameters)
           ).to eq(expected_payload)
         end
       end
