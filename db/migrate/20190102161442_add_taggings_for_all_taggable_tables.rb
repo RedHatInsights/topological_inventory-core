76f7a5d595e8462240c00415b40df7f0f9e2f103
Ladislav Smola <lsmola@redhat.com>
2019-01-04 13:20:56 +0100

    Add container group taggins

--- db/migrate/20190102161442_add_taggings_for_all_taggable_tables.rb
+++ db/migrate/20190102161442_add_taggings_for_all_taggable_tables.rb
@@ -21,7 +21,19 @@ class AddTaggingsForAllTaggableTables < ActiveRecord::Migration[5.1]
 
       t.references :vm, :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
 
-      t.index ["vm_id", "tag_id", "value"], :unique => true
+      t.index ["vm_id", "tag_id", "value"], :name => "uniq_index_on_vm_id_tag_id_and_value", :unique => true
+    end
+
+    create_table "container_groups_tags", id: :serial, force: :cascade do |t|
+      t.references :tenant, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :source, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :tag, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.string "value", :null => false, :default => ''
+
+      t.references :container_group, :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.index ["container_group_id", "tag_id", "value"], :name => "uniq_index_on_container_group_id_tag_id_and_value", :unique => true
     end
 
     # TODO add similar mapping tables for all other entities, containers_tags, container_images_tags, etc.
