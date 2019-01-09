a4af2ab49a8274e5771d0288e0ce28ccaab595c0
Ladislav Smola <lsmola@redhat.com>
2019-01-09 10:56:50 +0100

    Add rest of taggable container tables

--- db/migrate/20190102161442_add_taggings_for_all_taggable_tables.rb
+++ db/migrate/20190102161442_add_taggings_for_all_taggable_tables.rb
@@ -47,5 +47,41 @@ class AddTaggingsForAllTaggableTables < ActiveRecord::Migration[5.1]
 
       t.index ["container_image_id", "tag_id", "value"], :name => "uniq_index_on_container_image_id_tag_id_and_value", :unique => true
     end
+
+    create_table "container_node_tags", :id => :serial, :force => :cascade do |t|
+      t.references :tenant, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :source, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :tag, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.string "value", :null => false, :default => ''
+
+      t.references :container_node, :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.index ["container_node_id", "tag_id", "value"], :name => "uniq_index_on_container_node_id_tag_id_and_value", :unique => true
+    end
+
+    create_table "container_project_tags", :id => :serial, :force => :cascade do |t|
+      t.references :tenant, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :source, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :tag, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.string "value", :null => false, :default => ''
+
+      t.references :container_project, :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.index ["container_project_id", "tag_id", "value"], :name => "uniq_index_on_container_project_id_tag_id_and_value", :unique => true
+    end
+
+    create_table "container_template_tags", :id => :serial, :force => :cascade do |t|
+      t.references :tenant, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :source, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.references :tag, :type => :bigint, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.string "value", :null => false, :default => ''
+
+      t.references :container_template, :type => :bigint, :index => false, :null => false, :foreign_key => {:on_delete => :cascade}
+
+      t.index ["container_template_id", "tag_id", "value"], :name => "uniq_index_on_container_template_id_tag_id_and_value", :unique => true
+    end
   end
 end
