3bf8f39df9629f383e0e8e9a1985c967658afa3c
Ladislav Smola <lsmola@redhat.com>
2019-01-10 10:28:32 +0100

    Expose taggings for container images

--- app/models/container_image.rb
+++ app/models/container_image.rb
@@ -11,4 +11,14 @@ class ContainerImage < ApplicationRecord
   has_many :tags, :through => :container_image_tags
 
   acts_as_tenant(:tenant)
+
+  def taggings
+    container_image_tags.map do |tagging|
+      {
+        :tag_id => tagging.tag_id,
+        :name   => tagging.tag.name,
+        :value  => tagging.value,
+      }
+    end
+  end
 end
