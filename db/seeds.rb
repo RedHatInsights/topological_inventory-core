5732f5ca8fa1c0b3fefe9437add25f267c2e68b9
Ladislav Smola <lsmola@redhat.com>
2019-01-11 10:16:48 +0100

    Seed amazon source type

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 SourceType.find_or_create_by!(:name => "openshift", :product_name => "OpenShift", :vendor => "Red Hat")
+SourceType.find_or_create_by!(:name => "amazon", :product_name => "AWS", :vendor => "Amazon")
