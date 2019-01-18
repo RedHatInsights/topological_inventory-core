f0553d1ad5f9dd8adf9f7509a25c490c3d3d625b
Ladislav Smola <lsmola@redhat.com>
2019-01-18 08:52:31 +0100

    Avoid duplication for the defined relation

--- app/models/concerns/act_as_taggable_on.rb
+++ app/models/concerns/act_as_taggable_on.rb
@@ -1,17 +1,15 @@
 module ActAsTaggableOn
   def acts_as_taggable_on
-    mapping_table_name = "#{name.underscore}_tags".to_sym
-
     class_eval do
-      has_many mapping_table_name
-      has_many :tags, :through => mapping_table_name
+      def self.tagging_relation_name
+        "#{name.underscore}_tags".to_sym
+      end
+
+      has_many tagging_relation_name
+      has_many :tags, :through => tagging_relation_name
 
       def self.taggable?
         true
-      end
-
-      def self.tagging_relation_name
-        "#{name.underscore}_tags".to_sym
       end
 
       def taggings
