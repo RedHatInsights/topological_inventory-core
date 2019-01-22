809509e042260bc596cae5993e7593dc461d7f60
Ladislav Smola <lsmola@redhat.com>
2019-01-22 15:17:40 +0100

    Update or create based on :name
    This way we can modify the other attributes and they will just
    get updated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
-SourceType.find_or_create_by!(:name => "openshift", :product_name => "OpenShift", :vendor => "Red Hat")
-SourceType.find_or_create_by!(:name => "amazon", :product_name => "AWS", :vendor => "Amazon")
+def update_or_create(model, attributes)
+  obj = model.find_or_create_by!(:name => attributes[:name])
+  obj.update_attributes!(attributes.except(:name))
+end
+
+update_or_create(SourceType, :name => "openshift", :product_name => "OpenShift", :vendor => "Red Hat")
+update_or_create(SourceType, :name => "amazon", :product_name => "AWS", :vendor => "Amazon")
