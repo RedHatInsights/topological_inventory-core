a103aac02f3323e57e09277aaa1f699907699f81
Ladislav Smola <lsmola@redhat.com>
2019-01-22 15:51:45 +0100

    Test we don't have extraneous indexes

--- spec/topological_inventory/core/database_health.rb
+++ spec/topological_inventory/core/database_health.rb
@@ -2,6 +2,7 @@ require 'active_record_doctor'
 require "active_record_doctor/tasks"
 require 'active_record_doctor/tasks/missing_foreign_keys'
 require 'active_record_doctor/tasks/unindexed_foreign_keys'
+require 'active_record_doctor/tasks/extraneous_indexes'
 
 module TopologicalInventory
   module Core
@@ -14,6 +15,12 @@ module TopologicalInventory
 
       it "checks all foreign keys are covered by indexes" do
         result, _success = ActiveRecordDoctor::Tasks::UnindexedForeignKeys.run
+
+        expect(result).to be_empty
+      end
+
+      it "checks there are no extraneous indexes (for columns already covered by multi-column indexes)" do
+        result, _success = ActiveRecordDoctor::Tasks::ExtraneousIndexes.run
 
         expect(result).to be_empty
       end
