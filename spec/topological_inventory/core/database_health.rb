9c4dada80da8fc2dc73a1cfb41a1466a6e1deecb
Ladislav Smola <lsmola@redhat.com>
2019-01-22 13:37:26 +0100

    Test all foreign keys have indexes

--- spec/topological_inventory/core/database_health.rb
+++ spec/topological_inventory/core/database_health.rb
@@ -1,12 +1,19 @@
 require 'active_record_doctor'
 require "active_record_doctor/tasks"
 require 'active_record_doctor/tasks/missing_foreign_keys'
+require 'active_record_doctor/tasks/unindexed_foreign_keys'
 
 module TopologicalInventory
   module Core
     describe ApplicationRecord do
       it "checks that all foreign keys have foreign key constraint" do
         result, _success = ActiveRecordDoctor::Tasks::MissingForeignKeys.run
+
+        expect(result).to be_empty
+      end
+
+      it "checks all foreign keys are covered by indexes" do
+        result, _success = ActiveRecordDoctor::Tasks::UnindexedForeignKeys.run
 
         expect(result).to be_empty
       end
