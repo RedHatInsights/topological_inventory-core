fc72efea6a4129b761d918aecec91dcca9b4608d
Adam Grare <agrare@redhat.com>
2019-02-18 13:11:32 -0500

    Move value to tags table

--- db/schema.rb
+++ db/schema.rb
@@ -45,10 +45,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "container_group_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "container_group_id", null: false
-    t.index ["container_group_id", "tag_id", "value"], name: "uniq_index_on_container_group_id_tag_id_and_value", unique: true
     t.index ["tag_id"], name: "index_container_group_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_container_group_tags_on_tenant_id"
   end
@@ -83,10 +81,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "container_image_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "container_image_id", null: false
-    t.index ["container_image_id", "tag_id", "value"], name: "uniq_index_on_container_image_id_tag_id_and_value", unique: true
     t.index ["tag_id"], name: "index_container_image_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_container_image_tags_on_tenant_id"
   end
@@ -116,10 +112,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "container_node_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "container_node_id", null: false
-    t.index ["container_node_id", "tag_id", "value"], name: "uniq_index_on_container_node_id_tag_id_and_value", unique: true
     t.index ["tag_id"], name: "index_container_node_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_container_node_tags_on_tenant_id"
   end
@@ -155,10 +149,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "container_project_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "container_project_id", null: false
-    t.index ["container_project_id", "tag_id", "value"], name: "uniq_index_on_container_project_id_tag_id_and_value", unique: true
     t.index ["tag_id"], name: "index_container_project_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_container_project_tags_on_tenant_id"
   end
@@ -190,10 +182,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "container_template_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "container_template_id", null: false
-    t.index ["container_template_id", "tag_id", "value"], name: "uniq_index_on_container_template_id_tag_id_and_value", unique: true
     t.index ["tag_id"], name: "index_container_template_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_container_template_tags_on_tenant_id"
   end
@@ -375,10 +365,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "service_offering_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "service_offering_id", null: false
-    t.index ["service_offering_id", "tag_id", "value"], name: "uniq_index_on_service_offering_id_tag_id_and_value", unique: true
     t.index ["tag_id"], name: "index_service_offering_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_service_offering_tags_on_tenant_id"
   end
@@ -509,6 +497,7 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
     t.string "namespace", default: "", null: false
     t.text "description"
     t.datetime "created_at", null: false
+    t.string "value", default: "", null: false
     t.index ["tenant_id", "namespace", "name"], name: "index_tags_on_tenant_id_and_namespace_and_name", unique: true
   end
 
@@ -535,12 +524,10 @@ ActiveRecord::Schema.define(version: 2019_02_22_171042) do
   create_table "vm_tags", id: :serial, force: :cascade do |t|
     t.bigint "tenant_id", null: false
     t.bigint "tag_id", null: false
-    t.string "value", default: "", null: false
     t.datetime "created_at", null: false
     t.bigint "vm_id", null: false
     t.index ["tag_id"], name: "index_vm_tags_on_tag_id"
     t.index ["tenant_id"], name: "index_vm_tags_on_tenant_id"
-    t.index ["vm_id", "tag_id", "value"], name: "uniq_index_on_vm_id_tag_id_and_value", unique: true
   end
 
   create_table "vms", force: :cascade do |t|
