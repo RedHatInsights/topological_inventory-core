f1ddbfaf39d39fdcc8d90c2dd7c0ae3b44056273
Ladislav Smola <lsmola@redhat.com>
2019-02-28 10:13:42 +0100

    Make remove_reference on tenant_id revertible
    Although we need to drop the not_null constraint

--- db/migrate/20190218203848_remove_extra_columns_from_tagging_join_tables.rb
+++ db/migrate/20190218203848_remove_extra_columns_from_tagging_join_tables.rb
@@ -8,12 +8,26 @@ class RemoveExtraColumnsFromTaggingJoinTables < ActiveRecord::Migration[5.2]
     remove_column :service_offering_tags, :created_at, :datetime
     remove_column :vm_tags, :created_at, :datetime
 
-    remove_reference :container_group_tags, :tenant, :index => true, :null => false,  :foreign_key => {:on_delete => :cascade}
-    remove_reference :container_image_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
-    remove_reference :container_node_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
-    remove_reference :container_project_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
-    remove_reference :container_template_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
-    remove_reference :service_offering_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
-    remove_reference :vm_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+    reversible do |change|
+      change.up do
+        remove_reference :container_group_tags, :tenant, :index => true, :null => false,  :foreign_key => {:on_delete => :cascade}
+        remove_reference :container_image_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+        remove_reference :container_node_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+        remove_reference :container_project_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+        remove_reference :container_template_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+        remove_reference :service_offering_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+        remove_reference :vm_tags, :tenant, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      end
+
+      change.down do
+        add_reference :container_group_tags, :tenant, :index => true, :null => true,  :foreign_key => {:on_delete => :cascade}
+        add_reference :container_image_tags, :tenant, :index => true, :null => true, :foreign_key => {:on_delete => :cascade}
+        add_reference :container_node_tags, :tenant, :index => true, :null => true, :foreign_key => {:on_delete => :cascade}
+        add_reference :container_project_tags, :tenant, :index => true, :null => true, :foreign_key => {:on_delete => :cascade}
+        add_reference :container_template_tags, :tenant, :index => true, :null => true, :foreign_key => {:on_delete => :cascade}
+        add_reference :service_offering_tags, :tenant, :index => true, :null => true, :foreign_key => {:on_delete => :cascade}
+        add_reference :vm_tags, :tenant, :index => true, :null => true, :foreign_key => {:on_delete => :cascade}
+      end
+    end
   end
 end
