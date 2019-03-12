8257fcafd062e7312731fb87708471836d94649b
Adam Grare <agrare@redhat.com>
2019-03-12 11:49:40 -0400

    Add timestamps

--- db/migrate/20190308145549_add_applications.rb
+++ db/migrate/20190308145549_add_applications.rb
@@ -4,6 +4,7 @@ class AddApplications < ActiveRecord::Migration[5.2]
       t.references :tenant,           :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
       t.references :source,           :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
       t.references :application_type, :index => true, :null => false, :foreign_key => {:on_delete => :cascade}
+      t.timestamps
     end
   end
 end
