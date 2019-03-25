f1818afdf6524467adf962ee8cfb0f78b41c46a3
Ladislav Smola <lsmola@redhat.com>
2019-03-25 12:02:05 +0100

    external_tenant must be not null and unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_122542) do
+ActiveRecord::Schema.define(version: 2019_03_25_115642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -557,9 +557,10 @@ ActiveRecord::Schema.define(version: 2019_03_22_122542) do
   create_table "tenants", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "external_tenant"
+    t.string "external_tenant", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["external_tenant"], name: "index_tenants_on_external_tenant", unique: true
   end
 
   create_table "vm_tags", id: :serial, force: :cascade do |t|
