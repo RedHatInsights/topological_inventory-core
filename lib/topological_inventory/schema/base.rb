04e3811bb6a319e6181fb9edaf9ba1093e87498b
Martin Slemr <mslemr@redhat.com>
2019-05-24 17:50:51 +0200

    Schema class by name method

--- lib/topological_inventory/schema/base.rb
+++ lib/topological_inventory/schema/base.rb
@@ -12,6 +12,15 @@ module TopologicalInventory
           "source": manager.uid,
         }.merge(super)
       end
+
+      def self.klass_for(name)
+        klass_name = "TopologicalInventory::Schema::#{name}"
+        klass = klass_name.safe_constantize
+
+        # sometimes autoloader is confused and loads class with another namespace
+        # checks whether it loads what we want
+        klass if klass.to_s == klass_name
+      end
     end
   end
 end
