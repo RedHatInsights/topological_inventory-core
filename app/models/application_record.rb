21f7e18f5c27cd6d38642a712371d53753fd633a
Adam Grare <agrare@redhat.com>
2019-08-16 08:04:49 -0400

    Remove the Rails hack needed for acts_as_tenant
    Now that acts_as_tenant has released the gem with our fix we don't need
    this hack anymore.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,17 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
-  # Hack for running acts_as_tenant in a non Rails env.
-  # See https://github.com/ErwinM/acts_as_tenant/pull/192 for proposed fix
-  begin
-    module ::Rails
-      module VERSION
-        MAJOR = ActiveRecord::VERSION::MAJOR
-      end
-      def self.logger
-        require "topological_inventory/core/logging"
-        TopologicalInventory::Core.logger
-      end
-    end
-  end if !defined?(::Rails) || !::Rails.const_defined?("VERSION")
   require 'acts_as_tenant'
 
   self.abstract_class = true
