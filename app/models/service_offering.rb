6af3bb2d9223308763704b078286b70e2411e97a
Ladislav Smola <lsmola@redhat.com>
2019-10-02 15:21:43 +0200

    Add node models and relations

--- app/models/service_offering.rb
+++ app/models/service_offering.rb
@@ -8,9 +8,14 @@ class ServiceOffering < ApplicationRecord
   belongs_to :source
   belongs_to :source_region
   belongs_to :subscription
+  belongs_to :service_inventory
 
   has_many   :service_instances
   has_many   :service_plans
+
+  has_many :service_inventories
+  has_many :service_offering_nodes
+  has_many :child_service_offering_nodes, :class_name => "ServiceOfferingNode", :foreign_key => :root_service_offering
 
   acts_as_tenant(:tenant)
   acts_as_taggable_on
