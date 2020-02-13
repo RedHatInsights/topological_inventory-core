887a9aac3c7d3e20bf96b80022317197657cb57a
Libor Pichler <lpichler@redhat.com>
2020-02-13 16:23:01 +0100

    Add relations to ServiceCredentialType model

--- app/models/service_credential.rb
+++ app/models/service_credential.rb
@@ -6,5 +6,7 @@ class ServiceCredential < ApplicationRecord
   belongs_to :tenant
   belongs_to :source
 
+  belongs_to :service_credential_type, :optional => true
+
   acts_as_tenant(:tenant)
 end
--- app/models/service_credential_type.rb
+++ app/models/service_credential_type.rb
@@ -2,4 +2,11 @@ require "archived_concern"
 
 class ServiceCredentialType < ApplicationRecord
   include ArchivedConcern
+
+  belongs_to :tenant
+  belongs_to :source
+
+  has_many :service_credentials
+
+  acts_as_tenant(:tenant)
 end
