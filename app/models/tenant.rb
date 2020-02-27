df38b79347c54021af248bdfe84a98b28a7a3ee3
Libor Pichler <lpichler@redhat.com>
2020-02-27 12:48:24 +0100

    :Add missing *credential* relations to Tenant model

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -13,11 +13,17 @@ class Tenant < ApplicationRecord
   has_many :networks
   has_many :orchestration_stacks
   has_many :security_groups
+  has_many :service_credentials
+  has_many :service_credential_types
   has_many :service_instances
+  has_many :service_instance_service_credentials
   has_many :service_instance_nodes
+  has_many :service_instance_node_service_credentials
   has_many :service_inventories
   has_many :service_offerings
+  has_many :service_offering_service_credentials
   has_many :service_offering_nodes
+  has_many :service_offering_node_service_credentials
   has_many :service_offering_icons
   has_many :service_plans
   has_many :source_regions
